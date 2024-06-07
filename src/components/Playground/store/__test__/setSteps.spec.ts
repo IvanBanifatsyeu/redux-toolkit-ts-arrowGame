a31f2f5c03b38eb4dc65e71ea89913cb44f9322d
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-06-07 22:46:03 +0300

    SetSteps Vitest

--- src/components/Playground/store/__test__/setSteps.spec.ts
+++ src/components/Playground/store/__test__/setSteps.spec.ts
@@ -1,9 +1,27 @@
-import playgroundReducer, { initialState } from "../slices";
+import playgroundReducer, {
+  initialState,
+  setCurrentStep,
+  setSteps,
+} from "../slices";
 
-describe("", () => {
-  it("", () => {
-    const state = playgroundReducer({}, {});
+describe("reducer setSteps", () => {
+  it("check setSteps", () => {
+    const setCurrentStepState = playgroundReducer(
+      initialState,
+      setCurrentStep(),
+    );
 
-    expect().toEqual();
+    const setStepsSate = playgroundReducer(setCurrentStepState, setSteps());
+
+    const firstStep = {
+      step: 1,
+      currentValue: setStepsSate.steps[0].currentValue,
+      enteredValue: null,
+      success: null,
+    };
+
+    expect(initialState.steps.length).toBe(0);
+    expect(setStepsSate.steps.length).toBe(1);
+    expect(setStepsSate.steps[0]).toEqual(firstStep);
   });
 });
